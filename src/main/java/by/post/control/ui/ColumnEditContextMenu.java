8a676f3d2e10abc250a5bbae7c8ee9c0df3efc3a
Dmitiy V.Yefremov <satgluq@gmail.com>
2016-07-06 16:58:13 +0300

    clean

--- src/main/java/by/post/control/ui/ColumnEditContextMenu.java
+++ src/main/java/by/post/control/ui/ColumnEditContextMenu.java
@@ -1,11 +1,7 @@
 package by.post.control.ui;
 
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
 
 /**
  * @author Dmitriy V.Yefremov
@@ -18,7 +14,7 @@ public class ColumnEditContextMenu extends ContextMenu {
     public ColumnEditContextMenu() {
         init();
     }
-    
+
     private void init() {
 
         changeName = new MenuItem("Change name");
