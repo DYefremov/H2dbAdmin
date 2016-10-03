a528ce9393556accd66b5dd17a95c05e57447fb0
Dmitiy V.Yefremov <satgluq@gmail.com>
2016-10-03 14:12:18 +0300

    File explorer in recovery pane

--- src/main/java/by/post/control/ui/RecoveryPaneController.java
+++ src/main/java/by/post/control/ui/RecoveryPaneController.java
@@ -3,13 +3,14 @@ package by.post.control.ui;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -28,6 +29,10 @@ public class RecoveryPaneController {
     private TreeView<Path> databasePath;
     @FXML
     private TreeView<Path> savePath;
+    @FXML
+    private CheckBox filter;
+
+    private static final Logger logger = LogManager.getLogger(RecoveryPaneController.class);
 
     public RecoveryPaneController() {
 
@@ -36,31 +41,61 @@ public class RecoveryPaneController {
     @FXML
     public void onRun() {
 
+        String file = null;
+        String save = null;
+
+        TreeItem baseItem = databasePath.getSelectionModel().getSelectedItem();
+        TreeItem saveItem = savePath.getSelectionModel().getSelectedItem();
+
+        if (baseItem != null) {
+            file = String.valueOf(baseItem.getValue());
+        }
+
+        if (saveItem != null) {
+           save = String.valueOf(saveItem.getValue());
+        }
+
+        logger.info("Selected: database file = " + file + ",  path for save = " + save);
     }
 
     @FXML
     private void initialize() {
 
-        Iterable<Path> rootDirectories = FileSystems.getDefault().getRootDirectories();
-        TreeItem<Path> rootNode = null;
+        String hostName = null;
+
         try {
-            // Set name for root element by pc host name
-            rootNode = new TreeItem(InetAddress.getLocalHost().getHostName());
+            hostName =  InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
-            e.printStackTrace();
+            logger.error("RecoveryPaneController error [initialize]: " + e);
         }
 
-        for (Path name : rootDirectories) {
-            TreeItem treeNode = new TreeItem(name);
-            getTree(treeNode);
-            rootNode.getChildren().add(treeNode);
-            rootNode.setExpanded(true);
-        }
+        hostName = hostName == null ? "PC" : hostName;
 
-        databasePath.setRoot(rootNode);
-//        savePath.setRoot(rootNode);
-//        setListener(databasePath);
-//        setListener(savePath);
+        Iterable<Path> rootDirectories = FileSystems.getDefault().getRootDirectories();
+        TreeItem<Path> databaseItem = new TreeItem(hostName);
+        TreeItem<Path> saveItem = new TreeItem(hostName);
+
+        rootDirectories.forEach(name -> {
+            getRootNode(databaseItem, name);
+            getRootNode(saveItem, name);
+        });
+
+        databasePath.setRoot(databaseItem);
+        savePath.setRoot(saveItem);
+        setListener(databasePath);
+        setListener(savePath);
+    }
+
+    /**
+     * @param item
+     * @param name
+     */
+    private void getRootNode(TreeItem<Path> item, Path name) {
+
+        TreeItem newItem = new TreeItem(name);
+        newItem.setGraphic(getFolderImage());
+        item.getChildren().add(newItem);
+        item.setExpanded(true);
     }
 
     /**
@@ -71,18 +106,29 @@ public class RecoveryPaneController {
         view.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
             @Override
             public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                // Skip for root element
+                if (newValue.equals(view.getRoot())) {
+                    return;
+                }
 
                 TreeItem<Path> item = (TreeItem<Path>) newValue;
+                Path path = (Path) item.getValue();
 
-                if (Files.isDirectory(item.getValue())) {
-                    getTree(item);
-                    item.setExpanded(true);
+                if (Files.isDirectory(path)) {
+                    if (item.isLeaf()) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                getTree(item);
+                            }
+                        });
+                        item.setExpanded(true);
+                    }
                 } else {
-
+                    System.out.println(path.toFile().getAbsolutePath());
                 }
             }
         });
-
     }
 
     /**
@@ -98,43 +144,30 @@ public class RecoveryPaneController {
                 directoryStream.forEach(path -> {
 
                     TreeItem<Path> newItem = new TreeItem<>(path);
-                    item.getChildren().add(newItem);
 
                     if (Files.isDirectory(path)) {
-                        item.setGraphic(getFolderImage());
-                        item.addEventHandler(TreeItem.branchExpandedEvent(),new EventHandler() {
-                            @Override
-                            public void handle(Event event) {
-                                Platform.runLater(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        TreeItem it = (TreeItem) event.getSource();
-                                        if (it.isLeaf()) {
-//                                            newItem.setGraphic(getFolderImage());
-//                                            getTree(newItem);
-                                        }
+                        newItem.setGraphic(getFolderImage());
+                        item.getChildren().add(newItem);
+                        System.out.println(path);
+                    } else {
+                        boolean isDbFile = path.toFile().getName().toUpperCase().endsWith(".DB");
 
-                                    }
-                                });
-                            }
-                        });
+                        if (isDbFile || !filter.isSelected()) {
+                            item.getChildren().add(newItem);
+                        }
                     }
                 });
-
             }
-
         } catch (IOException e) {
-
+            logger.error("RecoveryPaneController error [getTree]: " + e);
         }
-
-
     }
 
     /**
      * @return view with folder image
      */
     private ImageView getFolderImage() {
-        return new ImageView(new Image("/img/folder_test.png", 25, 25, false, false));
+        return new ImageView(new Image("/img/folder_test.png", 16, 16, false, false));
     }
 
 }
